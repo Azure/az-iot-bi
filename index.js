9a1625a3ea8369e06e3d8be32751cb7ba1b6cc44
Kevin Zhao <kvn.zhl@gmail.com>
2016-10-14 16:36:07 +0800

    collect linux distro and refactor project structure (#13)
    * collect linux distro info
    
    * refinement
    
    * refactor code structure
    
    * simplify logic to resolve comments
    
    * minor update

--- index.js
+++ index.js
@@ -1,127 +1,27 @@
 'use strict';
 
 var config = require('./config.json');
-var crypto = require('crypto');
-var appInsight = require('applicationinsights');
-var settings = require('./lib/settings.js');
-var os = require('os');
-var getmacs = require('macaddress');
-
-// retrieve package info and parent package info
-require('pkginfo')(module, 'version');
-if (module.parent) {
-  require('pkginfo')(module.parent, 'name', 'version');
-}
-
-var currentPkgVersion = module.exports.version || '';
-var parentPkgName = module.parent && module.parent.exports.name ? module.parent.exports.name : '';
-var parentPkgVersion = module.parent && module.parent.exports.version ? module.parent.exports.version : '';
-
-var bi = {
-  _isStarted: false
-};
-
-var cacheObj = {};
-
-function hasValidMacProperty(obj, key) {
-  return obj &&
-    obj.hasOwnProperty(key) &&
-    obj[key].hasOwnProperty('mac') &&
-    obj[key].mac != '00:00:00:00:00:00';
-}
-
-function getSubscriptionAndTenantId() {
-  var profile = settings.readProfile();
-  if (profile.subscriptions && profile.subscriptions.constructor === Array && profile.subscriptions.length > 0) {
-    var subs = profile.subscriptions;
-    for (var i = 0; i < subs.length; i++) {
-      if (subs[i].hasOwnProperty('isDefault') && subs[i].isDefault === true) {
-        cacheObj.subscriptionId = subs[i].id;
-        cacheObj.tenantId = subs[i].tenantId;
-        return;
-      }
-    }
-  }
-}
+var wrapper = require('./lib/appInsightsWrapper.js');
+var bi = {};
 
 bi.start = function () {
   if (!config.instrumentationKey) {
     console.error('No instrumentation key found. Failed to start az-iot-bi.');
     return false;
   }
-
   if (process.env.TEST) {
     console.log('process.env.TEST is found. Do not start az-iot-bi for test environment.');
     return false;
   }
-
-  // Skip asking user's approval for data collection for preview release
-  if (settings.isBIEnabled(true) === true) {
-    appInsight.setup(config.instrumentationKey)
-      .setAutoCollectConsole(false)
-      .setAutoCollectExceptions(false)
-      .setAutoCollectPerformance(false)
-      .setAutoCollectRequests(false)
-      .setOfflineMode(true, 1)
-      .start();
-    bi._isStarted = true;
-    return true;
-  }
-  return false;
+  return wrapper.start(config.instrumentationKey);
 };
 
 bi.trackEvent = function (eventName, properties) {
-  if (!bi._isStarted) return;
-
-  if (!properties) {
-    properties = {};
-  }
-
-  // Add common properties
-  properties.parentModuleName = parentPkgName;
-  properties.parentModuleVersion = parentPkgVersion;
-  properties.packageVersion = currentPkgVersion;
-  properties.nodeVersion = process.version;
-  properties.osArch = os.arch();
-  properties.hostname = os.hostname();
-  properties.osPlatform = os.platform();
-  properties.osRelease = os.release();
-  properties.osType = os.type();
-
-  // Add subscription Id
-  if (!cacheObj.subscriptionId) {
-    getSubscriptionAndTenantId();
-  }
-  properties.subscriptionId = cacheObj.subscriptionId || '';
-  properties.tenantId = cacheObj.tenantId || '';
-
-  // Add MAC addresses
-  if (cacheObj.mac) {
-    properties.mac = cacheObj.mac;
-    appInsight.client.trackEvent(eventName, properties);
-  } else {
-    getmacs.all(function (err, all) {
-      if (!err && !cacheObj.mac) {
-        cacheObj.mac = [];
-        for (var key in all) {
-          if (hasValidMacProperty(all, key)) {
-            // TODO:
-            // Use MD5 for now. Will align with Azure-CLI later.
-            var md5sum = crypto.createHash('md5');
-            md5sum.update(all[key].mac);
-            cacheObj.mac.push(md5sum.digest('hex'));
-          }
-        }
-      }
-      properties.mac = cacheObj.mac || [];
-      appInsight.client.trackEvent(eventName, properties);
-    });
-  }
+  wrapper.trackEvent(eventName, properties);
 };
 
 bi.flush = function () {
-  if (!bi._isStarted) return;
-  appInsight.client.sendPendingData();
+  wrapper.sendPendingData();
 }
 
 module.exports = bi;
