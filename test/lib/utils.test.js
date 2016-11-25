44be01e7e833b5c3605797487ae21372ec6cf31a
Kevin Zhao <kvn.zhl@gmail.com>
2016-11-25 17:07:02 +0800

    remove more PII from BI data (#26)

--- test/lib/utils.test.js
+++ test/lib/utils.test.js
@@ -27,16 +27,29 @@ describe('lib/utils', function () {
         prop1: 'This property contains apple@boy.cat for testing.',
         prop2: 'This property contains 0.0.0.0 and 10.11.12.13 and 255.255.255.255 and 999.999.999.999 for testing.',
         prop3: 'This property contains no PII.',
-        prop4: 1234
+        prop4: 1234,
+        prop5: 'This property contains HostName=iot-hub.azure-devices.net;DeviceId=test-device;SharedAccessKeyName=iothubowner;SharedAccessKey=yJg53nhMLEjLq66z8+VcP82/UgGoY5abcdefQSuB9NnE for testing.', // eslint-disable-line
+        prop6: 'This property contains [Fake/Home/Folder] for testing.'
       };
       var expectedResult = {
         prop1: 'This property contains [Email Address] for testing.',
         prop2: 'This property contains [IP Address] and [IP Address] and [IP Address] and 999.999.999.999 for testing.',
         prop3: 'This property contains no PII.',
-        prop4: 1234
+        prop4: 1234,
+        prop5: 'This property contains [Host Name];[Device Id];[Shared Access Policy];[Shared Access Key] for testing.',
+        prop6: 'This property contains [Home Folder] for testing.'
       };
+
+      var oldUserProfile = process.env['USERPROFILE'];
+      var oldHome = process.env['HOME'];
+      process.env['USERPROFILE'] = '[Fake/Home/Folder]';
+      process.env['HOME'] = '[Fake/Home/Folder]';
+
       var actualResult = utils.sanitizePII(inputObj)
       assert.deepEqual(expectedResult, actualResult);
+
+      process.env['USERPROFILE'] = oldUserProfile || '';
+      process.env['HOME'] = oldHome || '';
     });
   });
 });
