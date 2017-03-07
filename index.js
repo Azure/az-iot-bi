b4b2071f38a31cd3b95228d3432e1ab43a9ba9c4
yungez <yungez@microsoft.com>
2017-03-07 16:06:12 +0800

    add customized appinsight key

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ var config = require('./config.json');
 var wrapper = require('./lib/appInsightsWrapper.js');
 var bi = {};
 
-bi.start = function (skipCheck, userBiSettingDir) {
-  if (!config.instrumentationKey) {
+bi.start = function (instrumentationKey, userBiSettingDir) {
+  var aiKey = '';
+  if (instrumentationKey) {
+    aiKey = instrumentationKey;
+  } else if (config.instrumentationKey) {
+    aiKey = config.instrumentationKey;    
+  } else {
     console.error('No instrumentation key found. Failed to start az-iot-bi.');
     return false;
   }
+
   if (process.env.TEST) {
     console.log('process.env.TEST is found. Do not start az-iot-bi for test environment.');
     return false;
   }
-  return wrapper.start(config.instrumentationKey, skipCheck, userBiSettingDir);
+  return wrapper.start(aiKey, userBiSettingDir);
 };
 
 bi.trackEvent = function (eventName, properties) {
